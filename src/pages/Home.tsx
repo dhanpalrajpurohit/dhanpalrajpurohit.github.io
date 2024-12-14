9493753425107a14feede1308fb13602c6d54da0
Dhanpal Singh <rajpurohitdhanpal@gmail.com>
2024-12-14 17:32:37 +0530

    fix: font family

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,39 +24,45 @@ const Home = () => {
 
           <div className={styles.emailBtn}>
             <Button style={{ backgroundColor: "black", width: '100%', textTransform: 'none', borderRadius: 0 }}
-              onClick={()=>{window.location.href = "mailto:rajpurohitdhanpal@gmail.com"}}
-            variant="contained">Email me</Button>
+              onClick={() => { window.location.href = "mailto:rajpurohitdhanpal@gmail.com" }}
+              variant="contained">Email me</Button>
           </div>
 
           <div className={styles.socialIcons}>
-            <IconButton aria-label="github" onClick={()=> window.open(
+            <IconButton aria-label="github" onClick={() => window.open(
               "https://github.com/dhanpalrajpurohit", "_blank"
             )}><GitHubIcon /></IconButton>
-            <IconButton aria-label="x-icon" onClick={()=> window.open(
+            <IconButton aria-label="x-icon" onClick={() => window.open(
               "https://x.com/DhanpalRaj1998", "_blank"
             )}><XIcon /></IconButton>
-            <IconButton aria-label="linked-icon" onClick={()=> window.open(
+            <IconButton aria-label="linked-icon" onClick={() => window.open(
               "https://www.linkedin.com/in/dhanpal-rajpurohit/", "_blank"
             )}><LinkedInIcon /></IconButton>
-                        
+
           </div>
         </div>
       </div>
 
       <div className={styles.rightSection}>
+
         <div className={styles.aboutMeText}>
-          <Typography variant="h2" component="h2" className={styles.aboutMe}>About Me</Typography>
+          <Typography variant="h2" component="h2" className={styles.aboutMe}>ABOUT ME</Typography>
           <Typography className={styles.aboutMeText}>
-            I am a highly skilled and tech-savvy professional with a Master's degree in Software Development, actively seeking a position as a Full-Stack Web Developer. With a strong passion for continuous learning and skill development, I have successfully contributed to a variety of projects, both independently and as part of a team.
+            I am a highly skilled and tech-savvy professional with a Master's degree in Software Development, 
+            actively seeking a position as a Full-Stack Web Developer. With a strong passion for continuous 
+            learning and skill development, I have successfully contributed to a variety of projects, both 
+            independently and as part of a team.
           </Typography>
         </div>
+
         <div className={styles.btnResumeContainer}>
           <Button className={styles.btnResume} style={{ backgroundColor: "black", textTransform: 'none', borderRadius: 0 }} variant="contained"
-            onClick={()=> window.open(
+            onClick={() => window.open(
               "https://drive.google.com/file/d/1iPT5YJ-IBhPV7E6DNUOAsFHrEX-i7XAe/view", "_blank"
             )}
           >Resume</Button>
         </div>
+
       </div>
     </div>
   )
